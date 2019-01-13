7fc1d1a61d97d03cd5db61ebe00e2113015fba2c
Simon <baeyenssimon@gmail.com>
2019-01-13 13:01:10 +0100

    Initial commit

---
+++ Hof Beygaert/Hof Beygaert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hof Beygaert
+//
+//  Created by Simon Baeyens on 12/01/2019.
+//  Copyright © 2019 HofBeygaert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Hof Beygaert/Hof BeygaertTests/Hof_BeygaertTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hof_BeygaertTests.swift
+//  Hof BeygaertTests
+//
+//  Created by Simon Baeyens on 12/01/2019.
+//  Copyright © 2019 HofBeygaert. All rights reserved.
+//
+
+import XCTest
+@testable import Hof_Beygaert
+
+class Hof_BeygaertTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
